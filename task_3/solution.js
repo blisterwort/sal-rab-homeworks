11684189217a4bef02acec2c5752532450eade05
Irina Tikhonova <blisterwort@yandex.ru>
2021-10-23 01:43:32 +0300

    task_3 complete

--- task_3/solution.js
+++ task_3/solution.js
@@ -3,17 +3,8 @@ function renderCartItem(item) {
 
     // Задание №3.1. Формирование строки корзины
 
-    let itemCountText;
-    itemCountText = ``
-    itemCountText += product.count;
-    itemCountText += ` × `;
-    itemCountText += product.price;
-    itemCountText += ` ₽ = `;
+    let itemCountText = `${product.count} × ${product.price} ₽ = ${product.count * product.price} ₽`
 
-    let sum = product.count * product.price;
-    itemCountText += sum;
-    itemCountText += ` ₽`;
-    
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
